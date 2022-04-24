e8e3c5fb86f4d7d8422a4029053099fbbf7915f7
5335510 <5335510@naver.com>
2022-04-24 13:19:49 +0900

    2022-04-24 13:19

--- src/com/javaex/ex07/Friend.java
+++ src/com/javaex/ex07/Friend.java
@@ -1,16 +1,35 @@
 package com.javaex.ex07;
 
 public class Friend {
-
+	//필드
     private String name;
     private String hp;
     private String school;
-
-    
+    //생성자
+ 
     // getter/setter 작성
+    public void setName(String name) {
+    	this.name = name;
+    }
+    public String getName() {
+    	return name;
+    }
     
+    public void setHp(String hp) {
+    	this.hp = hp;
+    }
+    public String getHp() {
+    	return hp;
+    }
     
+    public void setSchool(String school) {
+    	this.school = school;
+    }
+    public String getSchool() {
+    	return school;
+    }
     
+    //메소드-일반
     public void showInfo(){
         System.out.println("이름:"+name+"  핸드폰:"+hp+"  학교:"+school);
     }
--- src/com/javaex/ex07/FriendApp.java
+++ src/com/javaex/ex07/FriendApp.java
@@ -12,13 +12,22 @@ public class FriendApp {
         System.out.println("친구를 3명 등록해 주세요");
 
         //친구정보 3명 입력 로직 --> 반복문 사용
-            
+        for(int i = 0; i < friendArray.length; i++) {
+	        System.out.print("이름: ");
+	        
+	        System.out.print("핸드폰: ");
+	        
+	        System.out.print("학교: ");
+	        
+	        System.out.println("-------------------------");
+        }
         
-
+        
+        
         
         // 친구정보 출력
         for (int i = 0; i < friendArray.length; i++) {
-            //친구정보 출력 메소드 호출
+        	friendArray[i].showInfo();
         }
 
         sc.close();
