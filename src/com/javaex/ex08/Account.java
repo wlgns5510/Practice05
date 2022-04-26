ecf604de37fee85bdc19137fff755b1fe0a375ad
5335510 <5335510@naver.com>
2022-04-26 15:12:44 +0900

    2022-04-26 15:12

--- src/com/javaex/ex08/Account.java
+++ src/com/javaex/ex08/Account.java
@@ -1,12 +1,42 @@
 package com.javaex.ex08;
 
 public class Account {
-
+	
+	//필드
     private String accountNo;
     private int balance;
     
     //생성자 작성
-    
+    public Account() {}
+    public Account(String accountNo) {
+    	this.accountNo = accountNo;
+    }
     //필요한 메소드 작성
+    public String getAccountNo() {
+		return accountNo;
+	}
+	public void setAccountNo(String accountNo) {
+		this.accountNo = accountNo;
+	}
+	
+    
+    
+    
+    
+    
+    
+    public void deposit(int deposit) {
+		this.balance += deposit;
+		
+	}
+	
+	public void withdraw(int withdraw) {
+		this.balance -= withdraw;
+		
+	}
+	public void showBalance() {
+		System.out.println(balance);
+		
+	}
 
 }
--- src/com/javaex/ex08/AccountApp.java
+++ src/com/javaex/ex08/AccountApp.java
@@ -12,6 +12,7 @@ public class AccountApp {
         
         Account account = new Account("312-89562-123456");
         
+        System.out.println(account.getAccountNo() + "계좌가 개설되었습니다.");
         while(run){
             System.out.println("");
             System.out.println("----------------------------------");
